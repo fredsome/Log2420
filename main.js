8ecc411c9c060f19ae23c3b48cec564406808c09
freddy some <fredsome@hotmail.fr>
2020-02-11 22:11:06 -0600

    nouveau

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+
+var listbutton = document.getElementsByClassName('BouttonRadio');
+
+listbutton[0].addEventListener("click",add);
+listbutton[1].addEventListener("click",add);
+listbutton[2].addEventListener("click",add);
+listbutton[3].addEventListener("click",add);
+
+
+function add(){
+alert();
+
+}
