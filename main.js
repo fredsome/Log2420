e46309c74e91563a72d2a4c621ceb2de6b6b3e84
freddy some <fredsome@hotmail.fr>
2020-02-17 17:34:48 -0600

    Finalisation du partie polytique ,il ne reste plus que la partie Action

--- main.js
+++ main.js
@@ -15,14 +15,27 @@ fetch('https://log2420-serve.herokuapp.com/JSON/output.json')
   for (var i =0;i< data.Elections.length ;i++){
        document.getElementsByClassName('SousTitreEcole')[i].innerHTML = "<div>" + data.Elections[i].name + "</div>";
        document.getElementsByClassName('SousSoustitreEcole')[i].innerHTML = "<div>" + data.Elections[i].date + "</div>";
+   
+  }
+    for (var i =0;i< data.PartisProvinciaux.length ;i++){
+    
+      document.getElementsByClassName('SousTitreEcoleMillieu2')[i].innerHTML = "<div>" + data.PartisProvinciaux[i].abreviation + "</div>";
+      document.getElementsByClassName('SousSoustitreEcoleMilieu2')[i].innerHTML = "<div>" + data.PartisProvinciaux[i].fullname + "</div>";
     
     }
+    for (var i =0;i< data.PartisFederaux.length ;i++){
+    
+      document.getElementsByClassName('SousTitreEcoleMillieu')[i].innerHTML = "<div>" + data.PartisFederaux[i].abreviation + "</div>";
+      document.getElementsByClassName('SousSoustitreEcoleMilieu')[i].innerHTML = "<div>" + data.PartisFederaux[i].fullname + "</div>";
+    
+    }
+   
   })
   .catch(function (err) {
     console.log(err);
   });
 
-
+  var Titre = document.getElementById("miniTitre");
   var CanvasP = document.getElementById("typeprovincial");
   var CanvasF = document.getElementById("typeFederaux");
   var listbutton = document.getElementsByClassName('BouttonRadio');
@@ -31,15 +44,16 @@ listbutton[1].addEventListener("click",Provinciaux);
 listbutton[2].addEventListener("click",Federaux);
 listbutton[3].addEventListener("click",Provinciaux);
 
-typeFederaux
+var type;
 
 function Federaux(){
-  
+Titre.textContent ="Partis politiques fédéreaux"
     CanvasP.style.display ="none";
     CanvasF.style.display ="block";
 }
   
 function Provinciaux(){
+  Titre.textContent ="Partis politiques provinciaux"
   CanvasP.style.display ="block";
   CanvasF.style.display ="none";
   
